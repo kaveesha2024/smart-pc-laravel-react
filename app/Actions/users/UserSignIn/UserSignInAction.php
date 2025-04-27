f49fcfbc38d35ffc228d2bb9e066ac3cc3218d4f
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-04-27 20:44:26 +0530

    configured sanctum configurations and configured the user signIn method.

--- app/Actions/users/UserSignIn/UserSignInAction.php
+++ app/Actions/users/UserSignIn/UserSignInAction.php
@@ -2,7 +2,40 @@
 
 namespace App\Actions\users\UserSignIn;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
 class UserSignInAction
 {
+    public function __invoke($request): array
+    {
+        $user = User::where('email', $request['email'])->first();
 
+        if (!$user) {
+            return [
+                'status' => Response::HTTP_UNAUTHORIZED,
+                'message' => 'Invalid email address',
+            ];
+        }
+
+        if ($this->isUserAuthenticated($user, $request)) {
+            $token = $user->createToken('auth_token', ['server:admin'] )->plainTextToken;
+            return [
+                'status' => Response::HTTP_OK,
+                'message' => 'User logged in successfully',
+                'token' => $token,
+            ];
+        }
+
+        return [
+            'status' => Response::HTTP_UNAUTHORIZED,
+            'message' => 'Invalid password',
+        ];
+    }
+
+    private function isUserAuthenticated($user, $request): bool
+    {
+        return $request['email'] === $user->email && Hash::check($request['password'], $user->password);
+    }
 }
--- app/Http/Controllers/UserControlls/UserSignInController.php
+++ app/Http/Controllers/UserControlls/UserSignInController.php
@@ -2,19 +2,19 @@
 
 namespace App\Http\Controllers\UserControlls;
 
+use App\Actions\users\UserSignIn\UserSignInAction;
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserSignInController extends Controller
 {
-    public function userSignIn(Request $request): JsonResponse
+    public function userSignIn(Request $request, UserSignInAction $UserSignInAction): JsonResponse
     {
-        return response()->json([
-            'status' => Response::HTTP_OK,
-            'message' => 'User logged in successfully',
-        ]);
+        return response()->json($UserSignInAction($request));
     }
 }
