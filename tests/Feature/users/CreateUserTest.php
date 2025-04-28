a537be64aa6af5bb39b172d6838f91cbe8c0c483
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-04-28 10:04:16 +0530

    made a bad error test of create user test.

--- tests/Feature/users/CreateUserTest.php
+++ tests/Feature/users/CreateUserTest.php
@@ -17,7 +17,6 @@ final class CreateUserTest extends TestCase
     use RefreshDatabase;
 
     public function test_it_gets_a_successful_response_when_creating_a_new_user()
-
     {
         $user = [
             'first_name' => 'Kaveesha',
@@ -42,5 +41,33 @@ final class CreateUserTest extends TestCase
             'email' => 'sanaakaveesha@gmail.com',
             ]
         );
+    }
+
+    public function test_it_gets_a_bad_response_if_request_validations_are_failed()
+    {
+        $user = [
+            'first_name' => 'k',
+            'last_name' => 'Sashen',
+            'email' => 'sanaakaveeshagmail.com',
+            'password' => 'K',
+        ];
+        $response = $this->post('/api/users/user-signup', $user);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'status',
+            'errors',
+        ]);
+        $response->assertSimilarJson([
+            'status' => Response::HTTP_UNPROCESSABLE_ENTITY,
+            'errors' => [
+                'first_name' => ['The first name field must be at least 3 characters.'],
+                'email' => ['The email field must be a valid email address.'],
+                'password' => [
+                    'The password field must be at least 8 characters.',
+                    'The password field must contain at least one uppercase and one lowercase letter.',
+                    'The password field must contain at least one number.',
+                ],
+            ]
+        ]);
     }
 }
