7c6869ff3200a5c98f6580b1545bf2caabdc78ae
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-04-28 10:27:03 +0530

    create a test case to test 'request validation failed method'

--- tests/Feature/users/UserSignInTest.php
+++ tests/Feature/users/UserSignInTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\users;
 
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class UserSignInTest extends TestCase
@@ -25,4 +26,28 @@ class UserSignInTest extends TestCase
             'token',
         ]);
     }
+
+    public function test_it_gets_bad_response_if_request_validations_failed()
+    {
+        User::factory()->create();
+        $user = [
+            'email' => 'test123.com',
+            'password' => 'test'
+        ];
+        $response =  $this->post('/api/users/user-signin', $user);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'status',
+            'errors',
+        ]);
+        $response->assertSimilarJson([
+            'status' => Response::HTTP_UNAUTHORIZED,
+            'errors' => [
+                'email' => ['The email field must be a valid email address.'],
+                'password' => ['The password field must be at least 8 characters.'],
+            ],
+        ]);
+
+    }
+
 }
