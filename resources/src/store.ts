c48914d039e2f60175315b5c7bf72307804eee95
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-04-29 11:06:20 +0530

    persisted redux store.

--- resources/src/store.ts
+++ resources/src/store.ts
@@ -1,14 +1,15 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
+import authenticationSlice from "./utility/slices/authentication/authenticationSlice.ts";
 
 const persistConfig = {
     key: 'root',
     storage,
-    whitelist: ['authentication']
+    blacklist: ['authentication']
 }
 const rootReducer = combineReducers({
-    authentication: {}
+    authentication: authenticationSlice
 });
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
---
+++ resources/src/utility/slices/authentication/authenticationSlice.ts
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authenticationSlice = createSlice({
+    name: "AuthenticationSlice",
+    initialState: {
+        value: 'sashen(',
+    },
+    reducers: {},
+});
+export default authenticationSlice.reducer;
