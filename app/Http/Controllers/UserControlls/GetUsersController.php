89b565b94716a114183e3a47f33efc09a94099b2
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-04-30 12:36:35 +0530

    defined get all users action in getAllUsersAction.php file.

--- app/Http/Controllers/UserControlls/GetUsersController.php
+++ app/Http/Controllers/UserControlls/GetUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\UserControlls;
 
+use App\Actions\users\GetAllUsers\GetAllUsersAction;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
@@ -11,9 +12,8 @@ use Illuminate\Support\Facades\Log;
 
 class GetUsersController extends Controller
 {
-    public function getAllUsers(): JsonResponse
+    public function getAllUsers(GetAllUsersAction $GetAllUsers): JsonResponse
     {
-        $response = DB::table('users')->get();
-        return response()->json($response);
+        return response()->json($GetAllUsers());
     }
 }
