62156e41600a3d48adf138984d7477c89b478455
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-02 10:46:19 +0530

    created product seeder and config to create fake data.

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -17,7 +17,14 @@ class ProductFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'product_name' => $this->faker->name(),
+            'description' => $this->faker->text(),
+            'price' => $this->faker->randomFloat(2, 1000, 10000),
+            'image' => $this->faker->imageUrl(),
+            'category' => $this->faker->randomElement(['laptop', 'mobile', 'desktop', 'accessories']),
+            'brand' => $this->faker->word(),
+            'quantity' => $this->faker->numberBetween(1, 100),
+            'status' => $this->faker->randomElement(['in stock', 'out of stock']),
         ];
     }
 }
