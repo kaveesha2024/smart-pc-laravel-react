d514c38202e7be55105edf28452181f25f8510e0
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-06 13:29:07 +0530

    Add test for unauthorized user access to dashboard API

--- tests/Feature/dashboard/DashboardEndPointsTest.php
+++ tests/Feature/dashboard/DashboardEndPointsTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\dashboard;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 final class DashboardEndPointsTest extends TestCase
 
@@ -31,6 +32,28 @@ final class DashboardEndPointsTest extends TestCase
             'total_users',
             'total_products',
             'latest_signup_users'
+        ]);
+    }
+
+    public function test_it_gets_a_bad_response_if_a_user_try_to_call_dashboard_api()
+    {
+        $user = User::factory()->create()->toArray();
+
+        $loginResponse = $this->post('/api/users/user-signin', [
+            'email' => $user['email'],
+            'password' => 'Kaveesha123',
+        ]);
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '.$loginResponse->json()['token'],
+        ])->get('/api/dashboard');
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'status',
+            'message',
+        ]);
+        $response->assertSimilarJson([
+            'status' => Response::HTTP_UNAUTHORIZED,
+            'message' => 'you are not authorized to access this page',
         ]);
     }
 }
