27e70c007ca7b6446df5f9578fcb9d426a2f72c3
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-15 17:29:23 +0530

    created model, factory, seeder and migration for products ✏️.

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'product_name' => fake()->name(),
+            'product_id' => fake()->randomNumber(),
+            'description' => $this->faker->text(),
+            'card_description' => $this->faker->text(),
+            'price' => $this->faker->randomFloat(2, 100, 10000),
+            'labelled_price' => $this->faker->randomFloat(2, 100, 10000),
+            'discount' => $this->faker->randomFloat(2, 100, 10000),
+            'quantity' => $this->faker->numberBetween(0, 100),
+            'product_images' => $this->faker->imageUrl(),
+        ];
+    }
+}
