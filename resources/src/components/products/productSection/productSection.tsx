23025c919dd6a4c72fe1a9247f1f145de59167f4
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-17 17:21:40 +0530

    Refactor ProductSection to use updated product interface and integrate API for product fetching

--- resources/src/components/products/productSection/productSection.tsx
+++ resources/src/components/products/productSection/productSection.tsx
@@ -1,25 +1,31 @@
 import React  from "react";
 import 'aos/dist/aos.css';
 
+interface IProduct {
+    product_id: string;
+    product_name: string;
+    card_description: string;
+    product_images: string[];
+    price: string;
+    labelled_price: string;
+    description: string;
+    quantity: number;
+};
 
-interface IProductSection {
-    product: Array<{
-        name: string,
-        description: string,
-        image: string,
-    }>
-    category: string,
+interface IProductSectionProps {
+    products: IProduct[];
 }
-const ProductSection: React.FC <IProductSection> = ({ product, category }) => {
+
+const ProductSection: React.FC <IProductSectionProps> = ({ products }) => {
     return (
         <div className="p-6 bg-gray-100 min-h-screen">
-            <h2 className="text-2xl font-bold mb-6 text-center">{category}</h2>
+            {/* <h2 className="text-2xl font-bold mb-6 text-center">{category}</h2> */}
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-                {product.map((product, index) => (
+                {products.map((product: IProduct, index: number) => (
                     <div key={index} className="bg-white shadow-md rounded-2xl p-4 hover:shadow-lg transition">
-                        <img src={product.image} alt={product.name} className="w-full h-100 object-cover rounded-xl mb-4" />
-                        <h3 className="text-xl font-semibold mb-2">{product.name}</h3>
-                        <p className="text-gray-600 mb-4">{product.description}</p>
+                        <img src={product.product_images[0]} alt='image not found !' className="w-full h-100 object-cover rounded-xl mb-4" />
+                        <h3 className="text-xl font-semibold mb-2">{product.product_name}</h3>
+                        <p className="text-gray-600 mb-4">{product.card_description}</p>
                         <button className="px-4 py-2 bg-[#493628] text-white rounded  transition">View Details</button>
                     </div>
                 ))}
--- resources/src/pages/welcomePage/Welcome.tsx
+++ resources/src/pages/welcomePage/Welcome.tsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeroSection from "../../components/hero/HeroSection.tsx";
 import ProductSection from "../../components/products/productSection/productSection.tsx";
 import Footer from "../../components/footer/Footer.tsx";
+import axios from "axios";
 
 const Welcome:React.FC = () => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [products, setProducts] = useState([]);
+    useEffect(() => {
+        if (isLoading){
+            getAllProducts();
+            setIsLoading(false);
+            return;
+        }
+    }, []);
+    const getAllProducts = async () => {
+        try {
+            const response = await axios.get('/api/products');
+            if (response.data.status === 200) {
+                setProducts(response.data.products);
+                return
+            }
+            
+        }catch (error) {
+            console.error("Error fetching products:", error);
+        }
+    };
+    console.log(products);
+    
     const laptops = [
         {
             name: "ASUS VivoBook 15",
@@ -21,29 +45,29 @@ const Welcome:React.FC = () => {
             image: "https://www.laptop.lk/wp-content/uploads/2024/10/HP-Pavilion-x360-2-in-1-i5-05.webp",
         },
     ];
-    const mobiles = [
-        {
-            name: "Samsung Galaxy S24",
-            description: "A flagship Android phone with powerful performance and an amazing display.",
-            image: "https://celltronics.lk/wp-content/uploads/2024/01/Samsung-Galaxy-S24-Ultra-2.jpg", // Replace with actual image URL
-        },
-        {
-            name: "iPhone 15 Pro",
-            description: "Apple's latest device featuring the A17 chip and an enhanced camera system.",
-            image: "https://www.apple.com/newsroom/videos/iphone-15-pro-action-button/posters/Apple-iPhone-15-Pro-lineup-Action-button-230912.jpg.large_2x.jpg",
-        },
-        {
-            name: "OnePlus 12",
-            description: "Smooth performance, fast charging, and a clean software experience.",
-            image: "https://media.assettype.com/thequint%2F2023-12%2F8eb8fede-93da-4136-9f53-20e466ad81bb%2FOnePlus_12.webp",
-        },
-    ];
+    // const mobiles = [
+    //     {
+    //         name: "Samsung Galaxy S24",
+    //         description: "A flagship Android phone with powerful performance and an amazing display.",
+    //         image: "https://celltronics.lk/wp-content/uploads/2024/01/Samsung-Galaxy-S24-Ultra-2.jpg", // Replace with actual image URL
+    //     },
+    //     {
+    //         name: "iPhone 15 Pro",
+    //         description: "Apple's latest device featuring the A17 chip and an enhanced camera system.",
+    //         image: "https://www.apple.com/newsroom/videos/iphone-15-pro-action-button/posters/Apple-iPhone-15-Pro-lineup-Action-button-230912.jpg.large_2x.jpg",
+    //     },
+    //     {
+    //         name: "OnePlus 12",
+    //         description: "Smooth performance, fast charging, and a clean software experience.",
+    //         image: "https://media.assettype.com/thequint%2F2023-12%2F8eb8fede-93da-4136-9f53-20e466ad81bb%2FOnePlus_12.webp",
+    //     },
+    // ];
 
     return (
         <div>
             <HeroSection />
-            <ProductSection product={laptops} category="Laptop collection" />
-            <ProductSection product={mobiles} category='Mobile collection' />
+            <ProductSection products={products} />
+            {/* <ProductSection product={mobiles} category='Mobile collection' /> */}
             <Footer />
         </div>
     );
