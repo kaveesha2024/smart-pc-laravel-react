789eebf9726a5175eadd9b4215438378d67298df
Kaveesha Sashen <sanaakaveesha@gmail.com>
2025-05-21 06:48:28 +0530

    created test cases for request validation rules and authorization checkup ✏️.

--- tests/Feature/product/UpdateProductTest.php
+++ tests/Feature/product/UpdateProductTest.php
@@ -20,7 +20,7 @@ final class UpdateProductTest extends TestCase
             'role' => 'admin'
         ]);
         Sanctum::actingAs($user);
-        $res = $this->put('api/product/update/?productId=SMP00000000', [
+        $response = $this->put('api/product/update/?productId=SMP00000000', [
             "product_name" => "testProduct",
             "description" => "testDescription",
             "card_description" => "testCardDescription",
@@ -29,12 +29,12 @@ final class UpdateProductTest extends TestCase
             "quantity" => 2,
             "product_images" => $product['product_images']
         ]);
-        $res->assertStatus(200);
-        $res->assertJsonStructure([
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
             'status',
             'message',
         ]);
-        $res->assertSimilarJson([
+        $response->assertSimilarJson([
             'status' => 200,
             'message' => 'product updated successfully',
         ]);
@@ -46,6 +46,61 @@ final class UpdateProductTest extends TestCase
             "labelled_price" => 1000.99,
             "quantity" => 2,
             "product_images" => $product['product_images']
+        ]);
+    }
+    public function test_it_gets_a_bad_response_if_request_validations_failed()
+    {
+        $product = Product::factory()->create()->toArray();
+        $user = User::factory()->create([
+            'role' => 'admin'
+        ]);
+        Sanctum::actingAs($user);
+        $response = $this->put('api/product/update/?productId=SMP00000000', [
+            "product_name" => "",
+            "description" => 21,
+            "card_description" => "",
+            "price" => 1000.99,
+            "labelled_price" => 1000.99,
+            "quantity" => 2,
+            "product_images" => $product['product_images']
+        ]);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'status',
+            'errors',
+        ]);
+        $response->assertSimilarJson([
+            'status' => 422,
+            'errors' => [
+                'product_name' => ['The product name field is required.'],
+                'description' => ['The description field must be a string.'],
+                'card_description' => ['The card description field is required.']
+            ]
+        ]);
+    }
+
+    public function test_normal_user_is_unable_to_update_a_product()
+    {
+        $product = Product::factory()->create()->toArray();
+        $user = User::factory()->create();
+        Sanctum::actingAs($user);
+        $response = $this->put('api/product/update/?productId=SMP00000000', [
+            "product_name" => "",
+            "description" => 21,
+            "card_description" => "",
+            "price" => 1000.99,
+            "labelled_price" => 1000.99,
+            "quantity" => 2,
+            "product_images" => $product['product_images']
+        ]);
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'status',
+            'message',
+        ]);
+        $response->assertSimilarJson([
+            'status' => 401,
+            'message' => 'You are not authorized'
         ]);
     }
 }
